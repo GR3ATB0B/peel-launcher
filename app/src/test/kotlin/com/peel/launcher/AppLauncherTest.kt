a9a8cee30ce97a469f383c575fdc3f1f0174ed7b
Nash Vogeltanz <nashvogeltanz@gmail.com>
2026-05-04 18:16:50 -0400

    chore: pin Robolectric to SDK 34 via robolectric.properties
    Robolectric 4.13 caps at SDK 34 while compileSdk is 35. A per-class
    @Config(sdk = [34]) workaround was added in the previous commit; this
    moves the pin to a project-level robolectric.properties file so future
    test classes don't need the annotation.

--- app/src/test/kotlin/com/peel/launcher/AppLauncherTest.kt
+++ app/src/test/kotlin/com/peel/launcher/AppLauncherTest.kt
@@ -9,10 +9,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows.shadowOf
-import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [34])
 class AppLauncherTest {
 
     private val context: Context = ApplicationProvider.getApplicationContext()
